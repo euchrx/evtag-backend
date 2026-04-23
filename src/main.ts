6e1f87c5e0b3290d960b995e0737f4f4c1742a6b
Christian Evangelista <chriismatos@icloud.com>
2026-04-23 19:17:31 -0300

    update

--- src/main.ts
+++ src/main.ts
@@ -9,10 +9,7 @@ async function bootstrap() {
   app.enableCors({
     origin: [
       'http://localhost:5173',
-      'http://localhost:3000',
       'http://192.168.68.51:5173',
-      'http://192.168.68.51:3000',
-      'https://evtag-backend-production.up.railway.app',
       'https://evtag-frontend.vercel.app',
     ],
     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
